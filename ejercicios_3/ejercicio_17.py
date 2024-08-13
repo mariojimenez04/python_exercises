0713b601d7a653540d9cfd59a32347514c4f3209
mariojimenez04 <97321877+mariojimenez04@users.noreply.github.com>
2024-08-12 22:34:11 -0600

    .

---
+++ ejercicios_3/ejercicio_17.py
@@ -0,0 +1,5 @@
+diccionario_edades = {"Carlos":55, "María":42, "Mabel":78, "José":44, "Lucas":24, "Rocío":35, "Sebastián":19, "Catalina":2,"Darío":49}
+
+edad_minima = min(diccionario_edades.values())
+
+print(edad_minima)
---
+++ ejercicios_3/ejercicio_18.py
@@ -0,0 +1,5 @@
+lista_numeros = [44542247, 21310, 2134747, 44556475, 121676, 6654067, 353254, 123134, 552512, 611665]
+
+rango = min(lista_numeros) - max(lista_numeros)
+
+print(rango)
